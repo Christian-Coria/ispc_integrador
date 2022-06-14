2ed9455478abc6f28e6136d358118487cc9f9ebe
ChristianRodrigo77 <104704311+Christian-Coria@users.noreply.github.com>
2022-06-14 16:14:44 -0300

    Update Integrador.py

--- Integrador.py
+++ Integrador.py
@@ -10,23 +10,23 @@ class Facturando5Meses:
     def __init__(self,lista):
         self._lista = lista     #
     #encapsulamos el atributo de instancia
-    @property #permite obtener o usar el atributo en diferentes metodos
+    @property #permite obtener o usar el atributo en diferentes metodos con el decorador @property
     def lista(self):
         return self._lista
     
-    @lista.setter # damos acceso para asignar un valor fuera de la clase si asi se deseara
+    @lista.setter # damos acceso para asignar un valor fuera de la clase si asi se deseara con el decorador @setter
     def lista(self,lista):
         self._lista = lista
 
     def __str__(self): # el metodo __str__ devuelve la cadenacon los atributos que queremos mostrar si es llamado
         return f"Los Importes Vendidos en Cada Mes Ingresados a la Lista son: {self._lista}"
     
-    # Definimos la funcion para sumar todos los valores que incluye la lista
+    # Definimos el Metodo de la Clase para sumar todos los valores que incluye la lista
     def sumando_lista(self,lista):        
         Suma_de_lista = sum(lista) #creando funcion suma_de_lista sum()
         return Suma_de_lista
 
-    # Definimos la funcion para identificar el menor valor incluido en la lista
+    # Definimos el Metodo de la Clase para identificar el menor valor incluido en la lista
     def calculando_minimo(self,lista):
         min = lista[0] #asigno a min el valor del primer numero de la lista
         for i in range (1, len(lista)):
@@ -34,7 +34,7 @@ class Facturando5Meses:
                 min = lista[i] #se lo asignamos
             return min #por ultimo mandamos el numero final menor que quedo en la variable al programa general
 
-    # Definimos la funcion para identificar el mayor valor incluido en la lista
+    # Definimos el Metodo de la Clase para identificar el mayor valor incluido en la lista
     def calculando_maximo(self,lista):
         max = lista[0] #asigno a max el valor del primer numero de la lista
         for i in range (1, len(lista)):
@@ -42,7 +42,7 @@ class Facturando5Meses:
                 max = lista[i] # se lo asignamos 
             return max #por ultimo mandamos el numero final mayor que quedo en la variable al programa general
     
-    # Definimos la funcion promedio que devuelve el promedio de Facturacion de los periodos
+    # Definimos el Metodo de la Clase promedio que devuelve el promedio de Facturacion de los periodos
     def promediando(self,lista):
         return sum(lista) / len (lista)
 
@@ -80,25 +80,25 @@ else:
             opcion = int(input("Ingrese la Opcion Deseada: "))
         except Exception as e:
             print("Error: Debe ingresar solo numeros enteros y las opciones del Menu!")
-        else:
+        else: # si no hubo error entramos verificando la condicion de lo digitado para llamar al metodo de la clase que corresponda he imprimir el resultado.
             if opcion == 1:
-                print("La Suma total de ventas de los 5 periodos es: ",calculo1.sumando_lista(lista),"DOLARES")
+                print("La Suma total de ventas de los 5 periodos es: ",calculo1.sumando_lista(lista),"DOLARES\n")
             elif opcion == 2:
-                print("La venta menor de los 5 periodos es: ",calculo1.calculando_minimo(lista),"DOLARES")
+                print("La venta menor de los 5 periodos es: ",calculo1.calculando_minimo(lista),"DOLARES\n")
             elif opcion == 3:
-                print("La Mayor venta de los 5 periodos es: ", calculo1.calculando_maximo(lista),"DOLARES")
+                print("La Mayor venta de los 5 periodos es: ", calculo1.calculando_maximo(lista),"DOLARES\n")
             elif opcion == 4: 
-                print("El Promedio de ventas de los 5 periodos es: ", calculo1.promediando(lista),"DOLARES")
+                print("El Promedio de ventas de los 5 periodos es: ", calculo1.promediando(lista),"DOLARES\n")
             elif opcion == 5:
                 print("Consulta Realizada el ",datetime.now())
                 print("Saliendo-------\n")
                 break
             else:
-                print("Error opcion de Menu NO VALIDA")
-                #break
+                print("Error opcion de Menu NO VALIDA\n")
+                
 if __name__ == "__main__": # verificamos que todo lo que se ejecute se ejecute dentro del mismo modulo
     #saludamos al finalizar he informamos el horario y dia de la consulta
-    print("Gracias por Usar Nuestro SOFTWARE...")
+    print("Gracias por Usar Nuestro SOFTWARE...\n")
  
     
 
